e7a7960bb452051ee6018d839bca33513c7a34e7
Elvys Rodriguez <elvyster@gmail.com>
2025-12-31 02:07:42 -0400

    Otros cambios de la rama main

--- index.ts
+++ index.ts
@@ -1,8 +1,40 @@
+import { groqService } from "./services/groq";
+import type { AIService, ChatMessage } from "./types";
+
+const services: AIService[] = [groqService,
+// Add other services here
+]
+let currentServiceIndex = 0;
+
+//balancear entre servicios en cada solicitud
+function getNextService() {
+    const service = services[currentServiceIndex];
+    currentServiceIndex = (currentServiceIndex + 1) % services.length;
+    return service;
+}
+
 const server = Bun.serve({
     port: process.env.PORT ?? 3000,
-    async fetch(request) {
-        return new Response("API de BUN funcionando perfectamente!");
-    }
-});
+    async fetch(req) {
+        const { pathname } = new URL(req.url)
 
-console.log(`Servidor corriendo en http://localhost:${server.port}`);
+if (req.method === 'POST' && pathname === '/chat') {
+    const { messages } = await req.json() as { messages: ChatMessage[] };
+    const service = getNextService();
+    const stream = await service?.chat(messages)
+
+
+    return new Response(stream ,{
+        headers: {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive',
+    },
+    });
+}
+
+return new Response('Not Found', { status: 404 });
+    }
+})
+
+console.log(`Server running at http://localhost:${server.port}/`);
